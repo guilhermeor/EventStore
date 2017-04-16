2c21c5b70971f924631f492320e1c3436453bfdf
Guilherme Rosa <guilhermeor.rs@gmail.com>
2017-04-16 03:11:41 -0300

    Created initial structure containing interface to entities and initial class to store student information

---
+++ EventStoreExample/Domain/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Student : IEntity
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string RegistrationCode { get; set; }
+    }
+}
