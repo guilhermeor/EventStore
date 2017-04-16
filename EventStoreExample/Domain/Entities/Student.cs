0be44a1ddbb2b3c5b0bb50ac57a7bd08a639f7cf
Guilherme Rosa <guilhermeor.rs@gmail.com>
2017-04-16 16:29:42 -0300

    Removed unnecessary class. Added class to store a subject and logic information for student approval.

--- EventStoreExample/Domain/Entities/Student.cs
+++ EventStoreExample/Domain/Entities/Student.cs
@@ -1,6 +1,21 @@
-﻿namespace Domain.Entities
+﻿using Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Entities
 {
-    class Student
-    {
+    class Student : IEntity
+    {    
+        public int Id { get; private set; } 
+        public string Name { get; private set; } 
+        public int Age { get; private set; } 
+        public string RegistrationCode { get; set; }
+        public IList<Subject> Subjects { get; set; }
+        public bool Approved => ComputeApproval(Subjects);
+
+        private bool ComputeApproval(IList<Subject> subjects)
+        {
+            return !subjects.Any(x => x.LetterGrade.Equals('D'));
+        }
     }
 }
---
+++ EventStoreExample/Domain/Entities/Subject.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Entities
+{
+    class Subject
+    {
+        public string Name { get; private set; }
+        public double Grade { get; private set; }
+        public char LetterGrade => ComputeLetterGrade(Grade);
+
+        private char ComputeLetterGrade(double grade)
+        {
+            if (grade >= 9)
+                return 'A';
+            else if (grade >= 7.5)
+                return 'B';
+            else if (grade >= 6)
+                return 'C';
+            else
+                return 'D';
+        }
+    }
+}
